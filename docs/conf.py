1c5832dcc05cddc0c798b550797b7e03d36ee78c
지훈 김 <jhgim@idino.co.kr>
2025-10-28 23:12:42 +0900

    4

--- docs/conf.py
+++ docs/conf.py
@@ -135,10 +135,8 @@ todo_include_todos = False
 import os
 on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if not on_rtd:  # only import and set the theme if we're building docs locally
-    import sphinx_rtd_theme
-    html_theme = 'sphinx_rtd_theme'
-    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+html_theme = 'sphinx_rtd_theme'
+
 
 # otherwise, readthedocs.org uses their theme by default, so no need to specify it
 

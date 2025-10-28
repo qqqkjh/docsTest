107f90c05cbf026e351a2e441b1654e44ddb83e4
지훈 김 <jhgim@idino.co.kr>
2025-10-28 22:56:47 +0900

    1

--- docs/conf.py
+++ docs/conf.py
@@ -1,16 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
-
-# -- Project information
-
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
-
-release = '0.1'
-version = '0.1.0'
-
-# -- General configuration
-
+ 
 extensions = [
     'sphinx.ext.duration',
     'sphinx.ext.doctest',
@@ -19,17 +7,80 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
-intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
-    'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
+ templates_path = ['_templates']
+
+ 
+
+source_suffix = ['.rst']
+
+ 
+master_doc = 'index'
+
+# General information about the project.
+project = 'IdentityServer4'
+copyright = '2020, Brock Allen & Dominick Baier'
+author = 'Brock Allen & Dominick Baier'
+
+# The version info for the project you're documenting, acts as replacement for
+# |version| and |release|, also used in various other places throughout the
+# built documents.
+#
+# The short X.Y version.
+version = '1.0.0'
+# The full version, including alpha/beta/rc tags.
+release = '1.0.0'
+ 
+language = None
+
+ 
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+ 
+pygments_style = 'default'
+highlight_language = 'csharp'
+
+ 
+todo_include_todos = False
+
+
+ 
+import os
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+ 
+html_favicon = 'favicon.ico'
+ 
+html_static_path = ['_static']
+
+ 
+htmlhelp_basename = 'IdentityServer4doc'
+
+# -- Options for LaTeX output ---------------------------------------------
+
+latex_elements = {
+  
 }
-intersphinx_disabled_domains = ['std']
+ 
+latex_documents = [
+    (master_doc, 'IdentityServer4.tex', 'IdentityServer4 Documentation',
+     'Brock Allen, Dominick Baier', 'manual'),
+]
 
-templates_path = ['_templates']
+ 
+man_pages = [
+    (master_doc, 'identityserver4', 'IdentityServer4 Documentation',
+     [author], 1)
+]
+ 
+texinfo_documents = [
+    (master_doc, 'IdentityServer4', 'IdentityServer4 Documentation',
+     author, 'IdentityServer4', 'One line description of project.',
+     'Miscellaneous'),
+]
 
-# -- Options for HTML output
-
-html_theme = 'sphinx_rtd_theme'
-
-# -- Options for EPUB output
-epub_show_urls = 'footnote'
+ 
